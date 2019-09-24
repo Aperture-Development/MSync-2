9f75e204563e62c7021000d72dcdecba5f493656
Maximilian Grüttemeier <flisim2004@outlook.de>
2019-09-24 23:35:05 +0200

    Advanced Ban Info panel
    + Advanced ban info prototype

--- lua/msync/client_gui/modules/cl_mbsync.lua
+++ lua/msync/client_gui/modules/cl_mbsync.lua
@@ -200,6 +200,186 @@ MSync.modules[info.ModuleIdentifier].clientPanel = function()
         end
     end
 
+    MSync.modules[info.ModuleIdentifier].advancedInfoPanel = function()
+        local panel = vgui.Create( "DFrame" )
+        panel:SetSize( 350, 500 )
+        panel:SetTitle( "MBSync Advanced Ban Info" )
+        panel:Center()
+        panel:MakePopup()
+
+        --[[
+            Info about the banned user
+        ]]
+
+        local nickname_text = vgui.Create( "DLabel", panel )
+        nickname_text:SetPos( 15, 35 )
+        nickname_text:SetColor( Color( 255, 255, 255 ) )
+        nickname_text:SetText( "Nickname:" )
+        nickname_text:SetSize(380, 15)
+
+        local nickname_textentry = vgui.Create( "DTextEntry", panel )
+        nickname_textentry:SetPos( 125, 35 )
+        nickname_textentry:SetSize( 210, 20 )
+        nickname_textentry:SetText( "[ApDev] Rainbow Dash" )
+        nickname_textentry:SetDisabled(true)
+
+        local steamid_text = vgui.Create( "DLabel", panel )
+        steamid_text:SetPos( 15, 60 )
+        steamid_text:SetColor( Color( 255, 255, 255 ) )
+        steamid_text:SetText( "SteamID:" )
+        steamid_text:SetSize(380, 15)
+
+        local steamid_textentry = vgui.Create( "DTextEntry", panel )
+        steamid_textentry:SetPos( 125, 60 )
+        steamid_textentry:SetSize( 210, 20 )
+        steamid_textentry:SetText( "STEAM_0:0:0" )
+        steamid_textentry:SetDisabled(true)
+
+        local steamid64_text = vgui.Create( "DLabel", panel )
+        steamid64_text:SetPos( 15, 85 )
+        steamid64_text:SetColor( Color( 255, 255, 255 ) )
+        steamid64_text:SetText( "SteamID64:" )
+        steamid64_text:SetSize(380, 15)
+
+        local steamid64_textentry = vgui.Create( "DTextEntry", panel )
+        steamid64_textentry:SetPos( 125, 85 )
+        steamid64_textentry:SetSize( 210, 20 )
+        steamid64_textentry:SetText( "7600000000" )
+        steamid64_textentry:SetDisabled(true)
+
+        --local adminheader_text = vgui.Create( "DLabel", panel )
+        --adminheader_text:SetPos( 15, 110 )
+        --adminheader_text:SetColor( Color( 255, 255, 255 ) )
+        --adminheader_text:SetText( "Admin" )
+        --adminheader_text:SetSize(320, 15)
+        --adminheader_text:SetContentAlignment( 5 )
+
+        --[[
+            Info about the banning Admin
+        ]]
+
+        local adminnickname_text = vgui.Create( "DLabel", panel )
+        adminnickname_text:SetPos( 15, 120 )
+        adminnickname_text:SetColor( Color( 255, 255, 255 ) )
+        adminnickname_text:SetText( "Admin Nickname:" )
+        adminnickname_text:SetSize(380, 15)
+
+        local adminnickname_textentry = vgui.Create( "DTextEntry", panel )
+        adminnickname_textentry:SetPos( 125, 120 )
+        adminnickname_textentry:SetSize( 210, 20 )
+        adminnickname_textentry:SetText( "[ApDev] Rainbow Dash" )
+        adminnickname_textentry:SetDisabled(true)
+
+        local adminsteamid_text = vgui.Create( "DLabel", panel )
+        adminsteamid_text:SetPos( 15, 145 )
+        adminsteamid_text:SetColor( Color( 255, 255, 255 ) )
+        adminsteamid_text:SetText( "Admin SteamID:" )
+        adminsteamid_text:SetSize(380, 15)
+
+        local adminsteamid_textentry = vgui.Create( "DTextEntry", panel )
+        adminsteamid_textentry:SetPos( 125, 145 )
+        adminsteamid_textentry:SetSize( 210, 20 )
+        adminsteamid_textentry:SetText( "STEAM_0:0:0" )
+        adminsteamid_textentry:SetDisabled(true)
+
+        local adminsteamid64_text = vgui.Create( "DLabel", panel )
+        adminsteamid64_text:SetPos( 15, 170 )
+        adminsteamid64_text:SetColor( Color( 255, 255, 255 ) )
+        adminsteamid64_text:SetText( "Admin SteamID64:" )
+        adminsteamid64_text:SetSize(380, 15)
+
+        local adminsteamid64_textentry = vgui.Create( "DTextEntry", panel )
+        adminsteamid64_textentry:SetPos( 125, 170 )
+        adminsteamid64_textentry:SetSize( 210, 20 )
+        adminsteamid64_textentry:SetText( "7600000000" )
+        adminsteamid64_textentry:SetDisabled(true)
+
+        --[[
+            Info about the ban
+        ]]
+
+        local bandate_text = vgui.Create( "DLabel", panel )
+        bandate_text:SetPos( 15, 205 )
+        bandate_text:SetColor( Color( 255, 255, 255 ) )
+        bandate_text:SetText( "Ban Date:" )
+        bandate_text:SetSize(380, 15)
+
+        local bandate_textentry = vgui.Create( "DTextEntry", panel )
+        bandate_textentry:SetPos( 125, 205 )
+        bandate_textentry:SetSize( 210, 20 )
+        bandate_textentry:SetText( "24.09.2019 23:11" )
+        bandate_textentry:SetDisabled(true)
+
+        local banlength_text = vgui.Create( "DLabel", panel )
+        banlength_text:SetPos( 15, 230 )
+        banlength_text:SetColor( Color( 255, 255, 255 ) )
+        banlength_text:SetText( "Ban Length:" )
+        banlength_text:SetSize(380, 15)
+
+        local banlength_textentry = vgui.Create( "DTextEntry", panel )
+        banlength_textentry:SetPos( 125, 230 )
+        banlength_textentry:SetSize( 210, 20 )
+        banlength_textentry:SetText( "Permanent" )
+        banlength_textentry:SetDisabled(true)
+
+        local unbandate_text = vgui.Create( "DLabel", panel )
+        unbandate_text:SetPos( 15, 255 )
+        unbandate_text:SetColor( Color( 255, 255, 255 ) )
+        unbandate_text:SetText( "Unban Date:" )
+        unbandate_text:SetSize(380, 15)
+
+        local unbandate_textentry = vgui.Create( "DTextEntry", panel )
+        unbandate_textentry:SetPos( 125, 255 )
+        unbandate_textentry:SetSize( 210, 20 )
+        unbandate_textentry:SetText( "24.09.2019 23:11" )
+        unbandate_textentry:SetDisabled(true)
+
+        local remainingtime_text = vgui.Create( "DLabel", panel )
+        remainingtime_text:SetPos( 15, 280 )
+        remainingtime_text:SetColor( Color( 255, 255, 255 ) )
+        remainingtime_text:SetText( "Time Remaining:" )
+        remainingtime_text:SetSize(380, 15)
+
+        local remainingtime_textentry = vgui.Create( "DTextEntry", panel )
+        remainingtime_textentry:SetPos( 125, 280 )
+        remainingtime_textentry:SetSize( 210, 20 )
+        remainingtime_textentry:SetText( "1d,13h" )
+        remainingtime_textentry:SetDisabled(true)
+
+        local banreason_text = vgui.Create( "DLabel", panel )
+        banreason_text:SetPos( 15, 320 )
+        banreason_text:SetColor( Color( 255, 255, 255 ) )
+        banreason_text:SetText( "Ban Reason:" )
+        banreason_text:SetSize(380, 15)
+        banreason_text:SetDark(1)
+
+        local banreason_panel = vgui.Create( "DPanel", panel )
+        banreason_panel:SetPos( 15, 340 )
+        banreason_panel:SetSize( 320, 110 )
+
+        local banreasonreason_text = vgui.Create( "DLabel", banreason_panel )
+        banreasonreason_text:SetPos( 5, 5 )
+        banreasonreason_text:SetColor( Color( 0, 0, 0 ) )
+        banreasonreason_text:SetText( "This is a very long ban reason i am setting to see if the text wrapping does actually work. Maybe it doesnt this is why I test it. Please excuse my typos when you find this prototype commit" )
+        banreasonreason_text:SetSize(310, 100)
+        banreasonreason_text:SetWrap( true )
+        banreasonreason_text:SetContentAlignment( 7 )
+
+        local close_button = vgui.Create( "DButton", panel )
+        close_button:SetText( "Close" )	
+        close_button:SetPos( 15, 455 )
+        close_button:SetSize( 320, 30 )
+        close_button.DoClick = function() 
+            panel:Close()
+        end
+
+    end
+
+    MSync.modules[info.ModuleIdentifier].editBanPanel = function()
+
+    end
+
+    MSync.modules[info.ModuleIdentifier].advancedInfoPanel()
     return panel
 end
 
